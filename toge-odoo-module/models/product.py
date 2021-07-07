c3ab19f51567ab4422fc73802f94d3c5d6b1d7fa
alancecena <79159981+alancecena@users.noreply.github.com>
2021-07-07 11:39:05 -0500

    log product post data

--- toge-odoo-module/models/product.py
+++ toge-odoo-module/models/product.py
@@ -62,12 +62,16 @@ class ProductTemplate(models.Model):
     def upload_product_to_shopify(self):
         for line in self:
             upload_data = line.get_shopify_data_upload()
+            _logger.info(_("upload data %s") % upload_data.title)
             if upload_data:
                 headers = {'Content-Type': 'application/json'}
                 data_json = json.dumps({'params': upload_data})
-
+                
                 try:
                     shopify_product_upload_url = self.env.user.company_id.shopify_product_upload_url
+                    _logger.info(_("url %s") % shopify_product_upload_url)
+                    _logger.info(_("data %s") % data_json)
+                    _logger.info(_("headers %s") % headers)
                     requests.post(url=shopify_product_upload_url, data=data_json, headers=headers)
                 except Exception as e:
                     _logger.error(
